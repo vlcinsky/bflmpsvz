ede415f9b18cc70e1c8dbfb7037a45ccfd7107be
Jan Vlčinský <jan.vlcinsky@tamtamresearch.com>
2015-07-26 17:17:08 +0200

    Initial commit
            new file:   .gitignore
            new file:   README.rst
            new file:   bflmpsvz.py
            new file:   requirements.txt
            new file:   setup.cfg
            new file:   setup.py
            new file:   tox.ini

---
+++ README.rst
@@ -0,0 +1,5 @@
+========
+bflmpsvz
+========
+
+Stub package to test how multiple users can update the same project on pypi.
---
+++ bflmpsvz.py
@@ -0,0 +1,20 @@
+"""Print `blfmpsvz` plus user specified message
+
+Usage:
+    bflmpsvz <message>
+"""
+
+
+def main(msg=""):
+    print("bflmpsvz: " + msg)
+
+
+def script():
+    from docopt import docopt
+    args = docopt(__doc__)
+    msg = args["<message>"]
+    main(msg)
+
+
+if __name__ == "__main__":
+    script()
